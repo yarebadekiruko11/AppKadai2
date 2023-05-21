2d085d5ac3a80b0467adfb250ab824aa6ce12d17
yarebadekiruko11 <132581040+yarebadekiruko11@users.noreply.github.com>
2023-05-21 16:24:13 +0000

    Kadai2途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  post 'books' => 'books#create'
+
+  get '/books' => 'books#index'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get '/top' => 'homes#top'
+
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
